46f29e4544e658432342d84c727e1128cbff5a76
Julien Miotte <mike.perdide@gmail.com>
2011-01-24 13:36:51 +0100

    Moving the code used to launch qGitFilterBranch to __init__.py.

--- qGitFilterBranch/run.py
+++ qGitFilterBranch/run.py
@@ -2,40 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-from PyQt4.QtCore import QDir, QSettings
-from PyQt4.QtGui import QApplication, QFileDialog
-from qGitFilterBranch.main_window import MainWindow
-from os import environ
-from os.path import join, exists
-import sys
-
-def is_top_git_directory(filepath):
-    git_path = join(filepath, ".git")
-    return exists(git_path)
+from qGitFilterBranch import run
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    settings = QSettings("Noname company yet", "qGitFilterBranch")
-
-    settings.beginGroup("Last run")
-
-    filepath = '/'
-    while not is_top_git_directory(filepath):
-        filepath = unicode(QFileDialog.getExistingDirectory(
-            None,
-            "Open git repository",
-            unicode(settings.value("directory", QDir.homePath()).toString()),
-            QFileDialog.ShowDirsOnly
-            ))
-        if not filepath:
-            sys.exit(1)
-
-    settings.setValue("directory", filepath)
-    settings.endGroup()
-    settings.sync()
-
-    a = MainWindow(directory=filepath, debug=True)
-    a.show()
-    sys.exit(app.exec_())
-
+    run()
