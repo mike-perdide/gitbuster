d3a9749f9fb0ac15581246a79df231f5cd6dfdc0
Julien Miotte <mike.perdide@gmail.com>
2011-03-05 23:40:01 +0100

    Adding GPLv3 header to filter_main_class.py and graphics_item.py.

--- gitbuster/graphics_items.py
+++ gitbuster/graphics_items.py
@@ -1,3 +1,10 @@
+# graphics_item.py
+# Copyright (C) 2010 Julien Miotte <miotte.julien@gmail.com>
+#
+# This module is part of gitbuster and is released under the GPLv3
+# License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# -*- coding: utf-8 -*-
 
 class Arrow(QGraphicsObject, QGraphicsItem):
     """
