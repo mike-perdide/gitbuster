90a3dcfd9697636af9efc00add982497e10f269a
Julien Miotte <mike.perdide@gmail.com>
2011-03-05 23:40:01 +0100

    Adding GPLv3 header to filter_main_class.py and graphics_item.py.

--- gitbuster/filter_main_class.py
+++ gitbuster/filter_main_class.py
@@ -1,3 +1,11 @@
+# filter_main_class.py
+# Copyright (C) 2010 Julien Miotte <miotte.julien@gmail.com>
+#
+# This module is part of gitbuster and is released under the GPLv3
+# License: http://www.gnu.org/licenses/gpl-3.0.txt
+#
+# -*- coding: utf-8 -*-
+
 from PyQt4.QtGui import QMainWindow, QApplication, QCheckBox, QSpacerItem, \
                         QSizePolicy
 from PyQt4.QtCore import SIGNAL, QObject, Qt, QThread, QDir, QSettings, QDateTime
