979a5496faab79c65aafbeb9397e86530e892341
Julien Miotte <mike.perdide@gmail.com>
2011-05-23 13:45:52 +0200

    Fixing: "git ci" -> "git commit" (git ci is my alias, not everyone's)

--- tests/fake_git_gen.sh
+++ tests/fake_git_gen.sh
@@ -11,7 +11,7 @@ export GIT_AUTHOR_EMAIL="author-master@groom.com"
 git init
 echo "init" > file1
 git add file1
-git ci -a -m "Initial commit"
+git commit -a -m "Initial commit"
 git branch wallace_branch
 git co wallace_branch
 
@@ -19,11 +19,11 @@ export GIT_AUTHOR_NAME="Author 'Wallace' Groom"
 export GIT_AUTHOR_EMAIL="author-wallace@groom.com"
 
 echo "rooh" > file1
-git ci -a -m "Added rooh to file1 (was empty)"
+git commit -a -m "Added rooh to file1 (was empty)"
 echo "hello" > file1
-git ci -a -m "Replaced rooh with hello in file1"
+git commit -a -m "Replaced rooh with hello in file1"
 echo "cardboard" > file1
-git ci -a -m "Replaced hello with cardboard in file1"
+git commit -a -m "Replaced hello with cardboard in file1"
 
 git co master
 
@@ -31,20 +31,20 @@ export GIT_AUTHOR_NAME="Author 'Master' Groom"
 export GIT_AUTHOR_EMAIL="author-master@groom.com"
 
 echo "okay" > file1
-git ci -a -m "Added okay to file1 (was empty)"
+git commit -a -m "Added okay to file1 (was empty)"
 echo "hello" > file1
-git ci -a -m "Replaced okay with hello in file1"
+git commit -a -m "Replaced okay with hello in file1"
 echo "bobby" > file1
-git ci -a -m "Replaced hello with bobby in file1"
+git commit -a -m "Replaced hello with bobby in file1"
 
 echo "troutman" > file2
 git add file2
-git ci -a -m "Adding file2"
+git commit -a -m "Adding file2"
 echo "flint" > file2
-git ci -a -m "Replaced troutman with flint in file2"
+git commit -a -m "Replaced troutman with flint in file2"
 echo "rodney" > file2
 echo "jimmy" > file1
-git ci -a -m "Replaced flint with rodney in file2 and bobby with jimmy in file1"
+git commit -a -m "Replaced flint with rodney in file2 and bobby with jimmy in file1"
 git rm file2
 echo "scourge" > file1
-git ci -a -m "Removing file2 and replaced jimmy with scourge in file1"
+git commit -a -m "Removing file2 and replaced jimmy with scourge in file1"
