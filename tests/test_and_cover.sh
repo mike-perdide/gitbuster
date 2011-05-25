799822ac33362969cee39f91ddef866b2a9f733c
Julien Miotte <mike.perdide@gmail.com>
2011-05-25 14:22:23 +0200

    Script that does tests with coverage.

---
+++ tests/test_and_cover.sh
@@ -0,0 +1,7 @@
+GITBUSTER_DIR=$(python -c "import gitbuster; import os; print os.path.dirname(gitbuster.__file__)")
+GFBI_CORE_DIR=$(python -c "import gfbi_core; import os; print os.path.dirname(gfbi_core.__file__)")
+
+OMIT_UI_FILES="$GITBUSTER_DIR/*_ui.py"
+
+coverage run --source=$GITBUSTER_DIR,$GFBI_CORE_DIR --omit=$OMIT_UI_FILES tests_q_git_model.py
+coverage report --omit=$OMIT_UI_FILES
