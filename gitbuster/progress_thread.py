aa57320fd5a377dd197116bfa0db81a343a1609b
Feth AREZKI <feth@tuttu.info>
2011-05-25 09:54:44 +0200

    rm 'coding: utf8' not in 2 first l + unused imports
    the stance #coding: utf8 is not used by Python when not in the 2 first
    lines of a .py file

--- gitbuster/progress_thread.py
+++ gitbuster/progress_thread.py
@@ -4,10 +4,11 @@
 # This module is part of gitbuster and is released under the GPLv3
 # License: http://www.gnu.org/licenses/gpl-3.0.txt
 #
-# -*- coding: utf-8 -*-
+
 from PyQt4.QtCore import QThread, SIGNAL
 
 import time
+
 
 class ProgressThread(QThread):
     """
