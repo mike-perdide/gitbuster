2e7613db45de15645bee30dc97d036138819d0db
Julien Miotte <mike.perdide@gmail.com>
2011-06-09 19:48:56 +0200

    Fix #53: Validating the name in the new branch dialog.

--- gitbuster/branch_name_dialog.py
+++ gitbuster/branch_name_dialog.py
@@ -6,8 +6,13 @@
 #
 
 from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import QString, SIGNAL
 from gitbuster.branch_name_dialog_ui import Ui_Dialog
 
+from gfbi_core.validation import validate_branch_name
+
+RED = QString("background-color: red")
+WHITE = QString("background-color: white")
 
 class BranchNameDialog(QDialog):
 
@@ -17,5 +22,26 @@ class BranchNameDialog(QDialog):
         self._ui = Ui_Dialog()
         self._ui.setupUi(self)
 
+        self.connect_signals()
+
+    def connect_signals(self):
+        self.connect(self._ui.nameLineEdit,
+                     SIGNAL("textChanged (const QString&)"),
+                     self.check_name)
+
+    def check_name(self, potential_name):
+        try:
+            validate_branch_name(potential_name)
+            self._ui.nameLineEdit.setStyleSheet(WHITE)
+        except ValueError:
+            self._ui.nameLineEdit.setStyleSheet(RED)
+
     def get_new_name(self):
         return str(self._ui.nameLineEdit.text())
+
+    def accept(self):
+        try:
+            validate_branch_name(self._ui.nameLineEdit.text())
+            QDialog.accept(self)
+        except ValueError:
+            pass
