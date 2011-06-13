a340c7fe750f6ec03589cf5082edaadff11cec35
Julien Miotte <mike.perdide@gmail.com>
2011-06-13 21:00:42 +0200

    The model grapher is now able to work with the data() method.

--- tests/graph_model.py
+++ tests/graph_model.py
@@ -1,96 +1,68 @@
 from gfbi_core.util import Index
 from gfbi_core.editable_git_model import EditableGitModel
-from pprint import pprint
 
-total_branches = 1
-row = 0
 
-#def graph_model(model):
-#    head_commit = model.get_commits()[0]
-#    global parents_index
-#    parents_index = model.get_columns().index("parents")
-#    print_commit(model, head_commit)
+class ModelGrapher:
 
-def preprocessed_commits(model):
-    head_commit = model.get_commits()[0]
+    def __init__(self, model):
+        self._model = model
+        self._parents_index = model.get_columns().index("parents")
 
-    global preprocessed
-    preprocessed = []
-    prep_commit(head_commit)
+    def preprocess_commits(self, model):
+        head_commit = model.get_commits()[0]
 
-def prep_commit(commit, col=0):
-    global preprocessed
-    global row
-    global total_branches
+        preprocessed = []
+        self.prep_commit(head_commit, preprocessed)
+        return preprocessed
 
-    existing_row = [item for item in preprocessed if item and item[1] == commit]
-    if existing_row:
-#        newcol = existing_row[0][0]
-        preprocessed[preprocessed.index(existing_row[0])] = None
-    preprocessed.append((col, commit))
+    def prep_commit(self, commit, preprocessed, col=0):
+        existing_row = [item for item in preprocessed
+                        if item and item[1] == commit]
+        if existing_row:
+            preprocessed[preprocessed.index(existing_row[0])] = None
+        preprocessed.append((col, commit))
 
-    if len(commit.parents) != 1:
-        newcol = col + 1
-    else:
-        newcol = col
+        row = self._model.row_of(commit)
+        parents = self._model.data(Index(row, self._parents_index))
 
-    for _commit in reversed(commit.parents):
-        prep_commit(_commit, newcol)
-        newcol -= 1
+        if len(parents) != 1:
+            newcol = col + 1
+        else:
+            newcol = col
 
-def postprocess_commits():
-    for coord in preprocessed:
-        print coord
-        
+        for _commit in reversed(parents):
+            self.prep_commit(_commit, preprocessed, newcol)
+            newcol -= 1
 
-#def print_commit(model, commit, offset=0):
-#    global total_branches
-#
-#    row = model.row_of(commit)
-#    print_single(model, commit)
-#    sub_trees  = len(model.data(Index(row, parents_index)))
-#
-#    if sub_trees == 2:
-#        print '|\\'
-#        total_branches += 1
-#
-#    for _commit in model.data(Index(row, parents_index)):
-#        print_single(model, _commit, before=0, after=1)
-#
-#def print_single(model, commit, before=0, after=0):
-#    row = model.row_of(commit)
-#    print "| " * before + "o" + " |" * after + "\t\t\t" + \
-#            model.data(Index(row, 7)).split("\n")[0]
+    def graph_model(self):
+        preprocessed = self.preprocess_commits(self._model)
 
-preprocessed_commits(EditableGitModel("/tmp/tests_git"))
-#pprint(preprocessed)
+        awaiting_closure = {}
+        column = 0
+        commit = None
+        for coord in preprocessed:
+            if coord:
+                if coord[0] < column:
+                    awaiting_closure[column] = commit
+                column = coord[0]
+                commit = coord[1]
+                message = coord[1].message
+                display_string = "| " * (column) + "o"
 
-awaiting_closure = {}
-column = 0
-commit = None
-for coord in preprocessed:
-    if coord:
-        if coord[0] < column:
-            awaiting_closure[column] = commit
-        column = coord[0]
-        commit = coord[1]
-        message = coord[1].message
-#        print "===============", message.strip()
-        display_string = "| " * (column) + "o"
+                new_awaiting_closure = {}
+                for awaiting_col, awaiting_commit in awaiting_closure.items():
+                    if awaiting_commit.parents[0] == commit:
+                        display_string += "/ "
+                        awaiting_closure.pop(awaiting_col)
+                        if awaiting_closure:
+                            for awaiting_col in awaiting_closure:
+                                _com = awaiting_closure[awaiting_col]
+                                new_awaiting_closure[awaiting_col - 1] = _com
+                    else:
+                        display_string += " |"
 
-        new_awaiting_closure = {}
-        for awaiting_col, awaiting_commit in awaiting_closure.items():
-            if awaiting_commit.parents[0] == commit:
-#                print "closing", awaiting_commit.message.strip(), "on column", awaiting_col
-                display_string += "/ "
-                awaiting_closure.pop(awaiting_col)
-                if awaiting_closure:
-                    for awaiting_col in awaiting_closure:
-                        new_awaiting_closure[awaiting_col - 1] = awaiting_closure[awaiting_col]
-            else:
-                display_string += " |"
+                print display_string,
+                print "\t\t" + message.split("\n")[0]
 
-        print display_string,
-        print "\t\t" + message.split("\n")[0]
-
-#    print coord
+if __name__ == "__main__":
+    ModelGrapher(EditableGitModel("/tmp/tests_git")).graph_model()
